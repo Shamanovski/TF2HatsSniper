cb4bbbf52c20c4f72ce4a72e4f63a5e76fb1321b
shamanovsky <spike2@list.ru>
2020-10-13 22:20:43 +0300

    finish writing script which changes accounts if limited

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 'use strict';
 import fs from "fs";
-import GuardManager from "steam-totp";
+import { getAuthCode } from "steam-totp";
 import SteamCommunity from "steamcommunity";
-import request from "request";
+import requestPromise from "request-promise-native"
 import { URL } from "url";
 
 
 const skins = fs.readFileSync("LoggedSkins.txt", "utf-8").split("\n");
-const oldCache = JSON.parse(fs.readFileSync("old.cache", "utf-8"))
+let oldCache;
+try {
+    oldCache = JSON.parse(fs.readFileSync("cache.json", "utf-8"))
+}
+catch (e) {
+    oldCache = {}
+}
 
-const writeStream = fs.createWriteStream("new.cache", "utf-8", {flags: "w"});
+const writeStream = fs.createWriteStream("cache.json", "utf-8", {flags: "w"});
 const accounts = fs.readFileSync("accounts.txt", "utf-8").split("\n")
 const community = new SteamCommunity();
 let cache = {},
@@ -34,57 +40,90 @@ function getAccount() {
     const userOptions = {
         "accountName": login,
         "password": password,
-        "twoFactorCode": GuardManager.getAuthCode(sharedSecret)
+        "twoFactorCode": getAuthCode(sharedSecret),
+        "isLimited": false
     }
     return userOptions
 }
 
-async function main(login, password, twoFActorCode) {
-    return await new Promise ((resolve, reject) => {
-        community.login({accountName: login, password: password, twoFactorCode: twoFActorCode}, (err, sessionID, cookies, steamGuard) => {
-        request("POST", "https://steamcommunity.com/actions/SetLanguage/", { "language": "english", "sessionid": sessionID, timeout: 10 * 1000 })
-        for (var i = 0; i < skins.length; i++) {
-            let skin = "Unuaual" +  skins[i];
-            try {
-                link = `https://steamcommunity.com/market/listings/440/${skin}/render?start=0&count=100&currency=1&format=json`
-                response = request( "GET", link, { "sessionid": sessionID, timeout: 10 * 1000 })
-                response = JSON.parse(response.body);
-                listinginfo = response["listinginfo"];
-                for (let listing in listinginfo) {
-                    if (oldCache.hasOwnProperty(assetid)) continue;
-                    assetid = listinginfo[listing]["asset"]["id"];
-                    let descriptions = response["assets"]["440"]["2"][assetid]["descriptions"]
-                    let index;
-                    for (let k = 0 ; k < descriptions.length; k++) {
-                        if (descriptions[k]["value"].includes("Unusual")) {
-                            index = k
-                            break
-                        }
-                    }
-                    effect = descriptions[index]
-                    effectAndColor = (effect["value"] + " " + effect["color"])
-                    link = `https://steamcommunity.com/market/listings/440/${skin}?filter=${effect["value"]}`;
-                    price = listinginfo[listing]["price"] + listinginfo[listing]["fee"];
-                    console.log(`${skin}: ${price / 100}$ ${effectAndColor}\n${new URL(link).toString()}\n\n`);
-                    cache[assetid] = { "skin": skin, "price": listinginfo[listing]["price"] + listinginfo[listing]["fee"], "effect": effect };
-                    resolve("success")
-                }
-            }
-            catch  (ex) {
-                console.log(ex);
-                break;
-            }
-        }
-        writeStream.write(JSON.stringify(cache), "utf-8");
-        writeStream.close()
-        console.log(`Было проверено ${i} единиц`)
+async function mainProcess() {
+    let account = getAccount();
+    await new Promise((resolve, reject) => community.login({accountName: account.accountName, password: account.password, twoFactorCode: account.twoFactorCode}, 
+        (err, sessionID, cookies, steamGuard) => {
 
+            console.log(`Использую аккаунт ${account.accountName}`)
+            account.cookies = cookies;
+            account.sessionID = sessionID;
+            account.isLimited = false;
+            resolve()
         })
-    })
+    );
+    try {
+
+        for (var i = 0; i < skins.length; i++) {
+            if (account.isLimited == true) {
+                account = getAccount();
+                await new Promise((resolve, reject) => community.login({accountName: account.accountName, password: account.password, twoFactorCode: account.twoFactorCode}, 
+                    (err, sessionID, cookies, steamGuard) => {
+                        console.log(`Использую аккаунт ${account.accountName}`)
+                        account.cookies = cookies;
+                        account.sessionID = sessionID;
+                        account.isLimited = false;
+                        resolve()
+                    })
+                );
+            }   
+            let skin = "Unusual%20" + skins[i].replace(" ", "%20");
+            try {
+                link = `https://steamcommunity.com/market/listings/440/${skin}/render?start=0&count=100&currency=1&format=json`;
+                 response = await requestPromise({
+                     url: link,
+                     headers: {
+                        "sessionid": account.sessionID,
+                        "Cookie": account.cookies.join("; ")
+                    },
+                    timeout: 5 * 1000,
+                    transform: (body) => JSON.parse(body)
+                });
+                    
+                    listinginfo = response["listinginfo"];
+                    if (listinginfo.length == 0)
+                    {
+                        console.log(`Для ${skin.replace()} нет лотов`)
+                        continue;
+                    };
+                    let index;
+                    for (let listing in listinginfo) {
+                        if (oldCache.hasOwnProperty(assetid)) continue;
+                        assetid = listinginfo[listing]["asset"]["id"];
+                        let descriptions = response["assets"]["440"]["2"][assetid]["descriptions"]
+                        for (let k = 0 ; k < descriptions.length; k++) {
+                            if (descriptions[k]["value"].includes("Unusual")) {
+                                index = k
+                                break
+                            }
+                        }
+                        effect = descriptions[index];
+                        effectAndColor = (effect["value"] + " " + effect["color"])
+                        link = `https://steamcommunity.com/market/listings/440/${skin}?filter=${effect["value"]}`;
+                        skin = skin.replace("%20", " ");
+                        price = listinginfo[listing]["price"] + listinginfo[listing]["fee"];
+                        console.log(`${skin}: ${price / 100}$ ${effectAndColor}\n${new URL(link).toString()}\n\n`);
+                        cache[assetid] = { "skin": skin, "price": listinginfo[listing]["price"] + listinginfo[listing]["fee"], "effect": effect };
+                    }
+                }
+                catch  (ex) {
+                    console.log("Аккаунт залимичен. Жду...")
+                    await new Promise((res) => setTimeout(res, 120 * 1000))
+                    // account.isLimited = true;
+                    i -= 1
+                }
+        }
+    }
+
+    finally {
+        if (Object.keys(cache).length > 0) writeStream.write(JSON.stringify(cache), "utf-8");
+    }
 }
 
-let options;
-do {
-    options = getAccount();
-    main(options.accountName, options.password, options.twoFactorCode)
-} while (options.login != undefined)
+await mainProcess();
--- parser.js
+++ parser.js
@@ -1,14 +1,14 @@
-'use strict';
-const fs = require("fs");
-const request = require("request");
-const { parse } = require("node-html-parser")
+import fs from "fs";
+import { parse } from "node-html-parser"
+
 const stream = fs.createWriteStream("LoggedSkins.txt");
-const resp = request.get("https://backpack.tf/unusuals", (err, res, body) => {
-    const root = parse(body);
-    const selected = root.querySelectorAll(".tag.top-left")
-    let skinName;
-    for (let i = 0; i < selected.length; i++) {
-        skinName = selected[i].rawText;
-        stream.write(skinName + "\n")
-    }
-});
+const html = fs.readFileSync("skins.html", "utf-8")
+const root = parse(html.toString());
+const selected = root.querySelectorAll(".tag.top-left")
+let skinName;
+for (let i = 0; i < selected.length; i++) {
+    skinName = selected[i].rawText;
+    stream.write(skinName + "\n")
+}
+console.log("done")
+stream.close();
